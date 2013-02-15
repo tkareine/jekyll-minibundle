fafcda502ac091c27cd1820e82484172cca735fc
Tuomas Kareinen <tkareine@gmail.com>
2013-02-15 09:10:37 +0200

    Remove unneeded parentheses

--- test/unit/asset_bundle_test.rb
+++ test/unit/asset_bundle_test.rb
@@ -5,7 +5,7 @@ module Jekyll::Minibundle::Test
   class AssetBundleTest < TestCase
     def test_raise_exception_if_bundle_command_fails
       capture_io do
-        with_env('JEKYLL_MINIBUNDLE_CMD_JS' => 'false') do
+        with_env 'JEKYLL_MINIBUNDLE_CMD_JS' => 'false' do
           err = assert_raises(RuntimeError) { make_bundle }
           assert_equal 'Bundling js assets failed with exit status 1, command: false', err.to_s
         end
@@ -13,13 +13,13 @@ module Jekyll::Minibundle::Test
     end
 
     def test_raise_exception_if_bundle_command_not_found
-      with_env('JEKYLL_MINIBUNDLE_CMD_JS' => 'no-such-jekyll-minibundle-cmd') do
+      with_env 'JEKYLL_MINIBUNDLE_CMD_JS' => 'no-such-jekyll-minibundle-cmd' do
         assert_raises(Errno::ENOENT) { make_bundle }
       end
     end
 
     def test_raise_exception_if_bundle_command_not_configured
-      with_env('JEKYLL_MINIBUNDLE_CMD_JS' => nil) do
+      with_env 'JEKYLL_MINIBUNDLE_CMD_JS' => nil do
         err = assert_raises(RuntimeError) { make_bundle }
         assert_equal 'You need to set bundling command in $JEKYLL_MINIBUNDLE_CMD_JS', err.to_s
       end
