b2da42ec22a2aec8028415ff6568cd01c83fc54b
Tuomas Kareinen <tkareine@gmail.com>
2014-01-16 08:34:46 +0200

    Create asset stamp by reading file in chunks
    Because an asset file can be really big, we should avoid reading it into
    memory as a whole. We conserve memory by reading it in chunks.

--- lib/jekyll/minibundle/asset_stamp.rb
+++ lib/jekyll/minibundle/asset_stamp.rb
@@ -3,7 +3,7 @@ require 'digest/md5'
 module Jekyll::Minibundle
   module AssetStamp
     def self.from_file(path)
-      Digest::MD5.hexdigest(File.read(path))
+      Digest::MD5.file(path).hexdigest
     end
   end
 end
