31899569df1727c4e912791e0db44e7631545a9b
Tuomas Kareinen <tkareine@gmail.com>
2016-03-19 01:11:06 +0200

    Remove unnecessary leading slashes from error message

--- lib/jekyll/minibundle/mini_stamp_tag.rb
+++ lib/jekyll/minibundle/mini_stamp_tag.rb
@@ -6,10 +6,10 @@ module Jekyll::Minibundle
       super
       @asset_source, @asset_destination = text.split(/\s+/, 3)[0, 2]
       if !@asset_source || @asset_source.empty?
-        fail ArgumentError, "No asset source for ministamp tag; pass value such as '/_assets/site.css' as the first argument"
+        fail ArgumentError, "No asset source for ministamp tag; pass value such as '_assets/site.css' as the first argument"
       end
       if !@asset_destination || @asset_destination.empty?
-        fail ArgumentError, "No asset destination for ministamp tag; pass value such as '/assets/site.css' as the second argument"
+        fail ArgumentError, "No asset destination for ministamp tag; pass value such as 'assets/site.css' as the second argument"
       end
     end
 
--- test/unit/mini_stamp_tag_test.rb
+++ test/unit/mini_stamp_tag_test.rb
@@ -10,14 +10,14 @@ module Jekyll::Minibundle::Test
       err = assert_raises(ArgumentError) do
         Liquid::Template.parse("{% ministamp %}")
       end
-      assert_equal "No asset source for ministamp tag; pass value such as '/_assets/site.css' as the first argument", err.to_s
+      assert_equal "No asset source for ministamp tag; pass value such as '_assets/site.css' as the first argument", err.to_s
     end
 
     def test_raise_exception_if_no_asset_destination_argument
       err = assert_raises(ArgumentError) do
         Liquid::Template.parse("{% ministamp /_assets/site.css %}")
       end
-      assert_equal "No asset destination for ministamp tag; pass value such as '/assets/site.css' as the second argument", err.to_s
+      assert_equal "No asset destination for ministamp tag; pass value such as 'assets/site.css' as the second argument", err.to_s
     end
 
     def test_ignore_rest_arguments
