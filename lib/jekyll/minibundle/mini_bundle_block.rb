84ee7042b72f465e94e2cb09aab9d968adc26512
Tuomas Kareinen <tkareine@gmail.com>
2016-03-20 10:45:19 +0200

    Avoid registry cache element duplication with leading slashes

--- lib/jekyll/minibundle/mini_bundle_block.rb
+++ lib/jekyll/minibundle/mini_bundle_block.rb
@@ -36,14 +36,29 @@ module Jekyll::Minibundle
     private
 
     def get_current_bundle_config(local_bundle_config, site)
-      MiniBundleBlock.default_bundle_config.
+      config = MiniBundleBlock.default_bundle_config.
         merge(environment_bundle_config(site)).
         merge(local_bundle_config).
         merge('type' => @type)
+
+      baseurl, destination_path = normalize_baseurl_and_destination_path(config.fetch('baseurl'), config.fetch('destination_path'))
+
+      config.merge({'baseurl' => baseurl, 'destination_path' => destination_path})
     end
 
     def environment_bundle_config(site)
       { 'minifier_cmd' => Environment.minifier_command(site, @type) }
+    end
+
+    def normalize_baseurl_and_destination_path(baseurl, destination_path)
+      unless destination_path.start_with?('/')
+        return [baseurl, destination_path]
+      end
+
+      normalized_baseurl = baseurl.empty? ? '/' : baseurl
+      normalized_destination_path = destination_path.sub(/\A\/+/, '')
+
+      [normalized_baseurl, normalized_destination_path]
     end
   end
 end
--- lib/jekyll/minibundle/mini_stamp_tag.rb
+++ lib/jekyll/minibundle/mini_stamp_tag.rb
@@ -4,20 +4,35 @@ module Jekyll::Minibundle
   class MiniStampTag < Liquid::Tag
     def initialize(tag_name, text, _tokens)
       super
-      @asset_source, @asset_destination = text.split(/\s+/, 3)[0, 2]
-      if !@asset_source || @asset_source.empty?
+
+      @source_path, destination_path = text.split(/\s+/, 3)[0, 2]
+
+      if !@source_path || @source_path.empty?
         fail ArgumentError, "No asset source for ministamp tag; pass value such as '_assets/site.css' as the first argument"
       end
-      if !@asset_destination || @asset_destination.empty?
+
+      if !destination_path || destination_path.empty?
         fail ArgumentError, "No asset destination for ministamp tag; pass value such as 'assets/site.css' as the second argument"
       end
+
+      @baseurl, @destination_path = normalize_destination_path(destination_path)
     end
 
     def render(context)
       site = context.registers.fetch(:site)
-      file = AssetFileRegistry.stamp_file(site, @asset_source, @asset_destination)
+      file = AssetFileRegistry.stamp_file(site, @source_path, @destination_path)
       file.add_as_static_file_to(site)
-      file.destination_path_for_markup
+      @baseurl + file.destination_path_for_markup
+    end
+
+    private
+
+    def normalize_destination_path(destination_path)
+      if destination_path.start_with?('/')
+        ['/', destination_path.sub(/\A\/+/, '')]
+      else
+        ['', destination_path]
+      end
     end
   end
 end
