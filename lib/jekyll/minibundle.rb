2676305a02bd57fae38b2c3f020b708af1beead3
Tuomas Kareinen <tkareine@gmail.com>
2016-04-01 01:11:18 +0300

    Raise exception if Jekyll version isn't 3 or above

--- lib/jekyll/minibundle.rb
+++ lib/jekyll/minibundle.rb
@@ -1,3 +1,7 @@
+if defined?(Jekyll::VERSION) && Jekyll::VERSION < '3'
+  raise 'Minibundle plugin requires Jekyll version 3 or above'
+end
+
 require 'jekyll/minibundle/version'
 require 'jekyll/minibundle/mini_bundle_block'
 require 'jekyll/minibundle/mini_stamp_tag'
