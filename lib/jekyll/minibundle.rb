6ef9359122714613587d7f8632ff550b518e54d7
Tuomas Kareinen <tkareine@gmail.com>
2016-04-06 21:19:15 +0300

    Fix Jekyll version requirement check to be more reliable

--- lib/jekyll/minibundle.rb
+++ lib/jekyll/minibundle.rb
@@ -1,5 +1,7 @@
-if defined?(Jekyll::VERSION) && Jekyll::VERSION < '3'
-  raise 'Minibundle plugin requires Jekyll version 3 or above'
+if defined?(Jekyll::VERSION) &&
+   defined?(Gem::Version) &&
+   (Gem::Version.create(Jekyll::VERSION) < Gem::Version.create('3.0.0'))
+  raise 'Minibundle plugin requires Jekyll version >= 3.0.0'
 end
 
 require 'jekyll/minibundle/version'
